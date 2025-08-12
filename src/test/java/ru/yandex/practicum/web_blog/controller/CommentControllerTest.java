047736edcc8e8b3e268d24839ebbc65772239e0b
terokhovets <terokhovets@mdaudit.ru>
2025-08-12 13:54:12 +0300

    spring-boot. blog. v5. fix tests. final

--- src/test/java/ru/yandex/practicum/web_blog/controller/CommentControllerTest.java
+++ src/test/java/ru/yandex/practicum/web_blog/controller/CommentControllerTest.java
@@ -1,17 +1,12 @@
 package ru.yandex.practicum.web_blog.controller;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import ru.yandex.practicum.web_blog.service.CommentService;
 
@@ -21,7 +16,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest
 @WebMvcTest(CommentController.class)
 public class CommentControllerTest {
 
@@ -33,7 +27,6 @@ public class CommentControllerTest {
     //сохранение комментария
     @Test
     void createComment_shouldAddCommentToDatabaseAndRedirect() throws Exception {
-        //моккаем сервис
         Mockito.doNothing().when(commentService).save(any(), any());
         mockMvc.perform(post("/posts/{postId}/comments", 1)
                         .param("commentText", "Комментарий 1"))
@@ -44,7 +37,6 @@ public class CommentControllerTest {
     //обновление комментария
     @Test
     void updateComment_shouldEditCommentAndRedirect() throws Exception {
-        //моккаем сервис
         Mockito.doNothing().when(commentService).update(any(), any());
         mockMvc.perform(post("/posts/{postId}/comments/{id}", 1, 1)
                         .param("commentText", "Обновление комментария 1"))
@@ -55,7 +47,6 @@ public class CommentControllerTest {
     //удаление комментария
     @Test
     void deleteComment_shouldDeactivateCommentAndRedirect() throws Exception {
-        //моккаем сервис
         Mockito.doNothing().when(commentService).delete(any());
         mockMvc.perform(post("/posts/{postId}/comments/{id}/delete", 1, 1))
                 .andExpect(status().is3xxRedirection())
